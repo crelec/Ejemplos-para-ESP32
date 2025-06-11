9f1ddd9cc67814136158f17246298c9b4c8d74bb
César Augusto Romero Molano <111470363+crelec@users.noreply.github.com>
2025-06-11 04:51:45 -0500

    inicia1

--- Ejemplo_0/src/main.cpp
+++ Ejemplo_0/src/main.cpp
@@ -1,16 +1,18 @@
 #include <Arduino.h>
 
+#define LED_PIN 2
+
 void setup() {
-pinMode(2,OUTPUT);
+pinMode(LED_PIN,OUTPUT);
 Serial.begin(115200);
 }
 
 void loop() {
-digitalWrite(2, HIGH);
+digitalWrite(LED_PIN, HIGH);
 Serial.write("Encendido\n\r",11);
 //Serial.println("LED on");
 delay(1000);
-digitalWrite(2, LOW);
+digitalWrite(LED_PIN, LOW);
 Serial.println("LED off");
 delay(1000);
 }
